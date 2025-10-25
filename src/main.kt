4f04d9ad4258a3f6b4070c5d525df4e37e402854
rosenzhelyazkov <0zhelr65@solent.ac.uk>
2025-10-25 15:46:16 +0100

    add 2 more cats

--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,12 @@
 fun main() {
     val binnie = Cat("Binnie", 10, 10)
     val clyde = Cat("Clyde", 5, 5)
+    val Flathead= Cat("Flathead", 7, 10)
+    val Cupra= Cat("Cupra", 2, 7)
     binnie.walk()
     clyde.walk()
     println(binnie)
     println(clyde)
+    println(Flathead)
+    println(Cupra)
 }
