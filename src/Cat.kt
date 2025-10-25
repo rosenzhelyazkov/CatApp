da8dd85102611c60a22fd1cca3e545b4dc97168e
rosenzhelyazkov <0zhelr65@solent.ac.uk>
2025-10-25 16:04:30 +0100

    make the cats to walk and lose kilos

--- src/Cat.kt
+++ src/Cat.kt
@@ -1,14 +1,14 @@
 class Cat (val name: String, var age: Int, var weight: Int) {
 
-    fun walk() {
-        weight--
+    fun walk(times :Int=1) {
+        weight=weight-times
     }
-    fun eat(){
-        weight++
+    fun eat(times:Int=1) {
+        weight=weight+times
     }
 
     override fun toString() : String {
-        return "Name: $name Age: $age"
+        return "Name: $name Age: $age Weight: $weight"
 
     }
 }
