c8fa8982d45ec88cb95d873f7454964e7fbcf747
rosenzhelyazkov <0zhelr65@solent.ac.uk>
2025-10-25 16:27:58 +0100

    make the cats to walk and lose kilos

--- src/Cat.kt
+++ src/Cat.kt
@@ -1,5 +1,9 @@
 class Cat (val name: String, var age: Int, var weight: Int) {
-
+    var weight: Int=initialWeight
+        set(value) {
+            if(value in 0..20) field = value
+            else println("ERORR: weight must be between 0 and 20")
+        }
     fun walk(times :Int=1) {
         weight=weight-times
     }
